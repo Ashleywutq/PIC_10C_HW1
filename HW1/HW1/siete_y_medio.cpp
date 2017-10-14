01597f7bb0075286142cbd353831368d002641be
Ashley <Ashley@appledeMacBook-Pro-3.local>
2017-10-14 02:31:41 -0700

    import assignment code

---
+++ HW1/HW1/siete_y_medio.cpp
@@ -0,0 +1,30 @@
+//
+//  siete_y_medio.cpp
+//  HW1
+//
+//  some suggested libraries plus a stub for the main routine.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+    /* --STATEMENTS-- */
+    return 0;
+}
